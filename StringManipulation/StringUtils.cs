e05a938e84a8fe90cbca418df4c315e533d39cd0
Jack Hsu <jackhsu0225@gmail.com>
2020-02-10 23:17:49 -0500

    (1) Added StringManipulation project. (2) Added 1st attempt of the ReverseString function.

---
+++ StringManipulation/StringUtils.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulation
+{
+    public class StringUtils
+    {
+        public static string ReverseString(string input)
+        {
+            if (input == null)
+                return "";
+
+            var arr = input.ToCharArray();
+            var center = arr.Length / 2;
+            var last = arr.Length - 1;
+
+            if(center % 2 == 0)
+            {
+                for (int i = 0; i < center; i++)
+                {
+                    var temp = arr[i];
+                    arr[i] = arr[last];
+                    arr[last] = temp;
+                    
+                    last--;
+                }
+            }
+            else
+            {
+                for (int i = 0; i <= center; i++)
+                {
+                    var temp = arr[i];
+                    arr[i] = arr[last];
+                    arr[last] = temp;
+
+                    last--;
+                }
+            }
+
+            return new string(arr);
+        }
+    }
+}
