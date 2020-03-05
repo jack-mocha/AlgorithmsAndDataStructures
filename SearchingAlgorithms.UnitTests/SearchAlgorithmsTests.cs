9a949839a4468079ffad36db8e93c862b75c20ac
Jack Hsu <jackhsu0225@gmail.com>
2020-03-04 21:38:06 -0500

    Added LinearSearch method

---
+++ SearchingAlgorithms.UnitTests/SearchAlgorithmsTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SearchingAlgorithms.UnitTests
+{
+    [TestFixture]
+    public class SearchAlgorithmsTests
+    {
+        private Search _search;
+
+        [SetUp]
+        public void Setup()
+        {
+            _search = new Search();
+        }
+
+        [Test]
+        [TestCase(null, 4, -1)]
+        [TestCase(new int[] { }, 4, -1)]
+        [TestCase(new int[] { 1,2,3,4 }, 4, 3)]
+        [TestCase(new int[] { 1,2,3,4 }, 5, -1)]
+        public void LinearSearch_WhenCalled_ReturnsTargetIndexInTheArray(int[] array, int target, int expectedResult)
+        {
+            var result = _search.LinearSearch(array, target);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
---
+++ SearchingAlgorithms/Search.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchingAlgorithms
+{
+    public class Search
+    {
+        public int LinearSearch(int[] array, int target)
+        {
+            if (array == null)
+                return -1;
+            if (array.Length == 0)
+                return -1;
+
+            for(int i=0; i<array.Length; i++)
+            {
+                if (array[i] == target)
+                    return i;
+            }
+
+            return -1;
+        }
+    }
+}
