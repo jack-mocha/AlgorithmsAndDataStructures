b1d1d3bbc7aa1b40878b2c0afc6204535a262350
Jack Hsu <jackhsu0225@gmail.com>
2020-03-05 23:25:34 -0500

    [1] Added BubbleSort class [2] Added SortingAlgorithms.UnitTest project

---
+++ SortingAlgorithms.UnitTests/BubbleSortTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SortingAlgorithms.UnitTests
+{
+    [TestFixture]
+    public class BubbleSortTests
+    {
+        private BubbleSort _bubbleSort;
+
+        [SetUp]
+        public void Setup()
+        {
+            _bubbleSort = new BubbleSort();
+        }
+
+        [Test]
+        [TestCase(null, null)]
+        [TestCase(new int[] { }, new int[] { })]
+        [TestCase(new int[] { 1 }, new int[] { 1 })]
+        [TestCase(new int[] {9, 8, 4, 1}, new int[] {1, 4, 8, 9})]
+        [TestCase(new int[] {9, 4, 4, 1}, new int[] {1, 4, 4, 9})]
+        [TestCase(new int[] {1, 2, 3, 4}, new int[] {1, 2, 3, 4})]
+        public void SortAsc_WhenCalled_SortArrayAsc(int[] array, int[] expectedResult)
+        {
+            _bubbleSort.SortAsc(array);
+
+            Assert.That(array, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        [TestCase(null, null)]
+        [TestCase(new int[] { }, new int[] { })]
+        [TestCase(new int[] { 1 }, new int[] { 1 })]
+        [TestCase(new int[] { 9, 8, 4, 1 }, new int[] { 9, 8, 4, 1 })]
+        [TestCase(new int[] { 1, 3, 3, 4 }, new int[] { 4, 3, 3, 1 })]
+        public void SortDesc_WhenCalled_SortArrayAsc(int[] array, int[] expectedResult)
+        {
+            _bubbleSort.SortDesc(array);
+
+            Assert.That(array, Is.EqualTo(expectedResult));
+        }
+    }
+}
---
+++ SortingAlgorithms/BubbleSort.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    public class BubbleSort
+    {
+        /// <summary>
+        /// Every iteration, we bring the next biggest item to the correct position.
+        /// 
+        /// Time Complexity:
+        ///     Best Case: O(1) pass * O(n) comparison = O(n)
+        ///     Worst Case: O(n) pass * O(n) comparison = O of n square
+        ///     
+        /// Without optimization, it is always O of n square.
+        /// </summary>
+        /// <param name="array"></param>
+        public void SortAsc(int[] array)
+        {
+            if (array == null)
+                return;
+
+            var isSorted = true; //Optimization1: if the array is already sorted, we don't need to compare elements anymore.
+
+            for(int i = 0; i < array.Length; i++) //array.Length of iterations
+            {
+                isSorted = true;
+                for(int j = 1; j < array.Length - i; j++) //Optimization2: We don't need to compare those which are already sorted.
+                {
+                    if (array[j] < array[j - 1])
+                    {
+                        Swap(array, j, j - 1);
+                        isSorted = false;
+                    }
+                }
+
+                if (isSorted)
+                    return;
+            }
+        }
+
+        public void SortDesc(int[] array)
+        {
+            if (array == null)
+                return;
+
+            var isSorted = true; //Optimization1: if the array is already sorted, we don't need to compare elements anymore.
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                isSorted = true;
+                for (int j = 1; j < array.Length - i; j++) //Optimization2: We don't need to compare those which are already sorted.
+                {
+                    if (array[j] > array[j - 1])
+                    {
+                        Swap(array, j, j - 1);
+                        isSorted = false;
+                    }
+                }
+
+                if (isSorted)
+                    return;
+            }
+        }
+
+        private void Swap(int[] array, int idx1, int idx2)
+        {
+            var temp = array[idx1];
+            array[idx1] = array[idx2];
+            array[idx2] = temp;
+        }
+    }
+}
