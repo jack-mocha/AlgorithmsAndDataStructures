80ad1de8cd56084531be332c4bbbd7036004e65b
Jack Hsu <jackhsu0225@gmail.com>
2020-03-07 22:26:35 -0500

    Added InsertionSort methods.

---
+++ SortingAlgorithms.UnitTests/InsertionSortTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using NUnit.Framework;
+namespace SortingAlgorithms.UnitTests
+{
+    [TestFixture]
+    public class InsertionSortTests
+    {
+        private InsertionSort _insertionSort;
+        [SetUp]
+        public void Setup()
+        {
+            _insertionSort = new InsertionSort();
+        }
+
+        [Test]
+        [TestCase(null, null)]
+        [TestCase(new int[] { }, new int[] { })]
+        [TestCase(new int[] { 1 }, new int[] { 1 })]
+        [TestCase(new int[] { 9, 8, 4, 1 }, new int[] { 1, 4, 8, 9 })]
+        [TestCase(new int[] { 9, 4, 4, 1 }, new int[] { 1, 4, 4, 9 })]
+        [TestCase(new int[] { 1, 2, 3, 4 }, new int[] { 1, 2, 3, 4 })]
+        public void SortAsc_WhenCalled_SortArrayAsc(int[] array, int[] expectedResult)
+        {
+            _insertionSort.SortAsc(array);
+
+            Assert.That(array, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        [TestCase(null, null)]
+        [TestCase(new int[] { }, new int[] { })]
+        [TestCase(new int[] { 1 }, new int[] { 1 })]
+        [TestCase(new int[] { 9, 8, 4, 1 }, new int[] { 1, 4, 8, 9 })]
+        [TestCase(new int[] { 9, 4, 4, 1 }, new int[] { 1, 4, 4, 9 })]
+        [TestCase(new int[] { 1, 2, 3, 4 }, new int[] { 1, 2, 3, 4 })]
+        public void SortAsc2_WhenCalled_SortArrayAsc(int[] array, int[] expectedResult)
+        {
+            _insertionSort.SortAsc2(array);
+
+            Assert.That(array, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        [TestCase(null, null)]
+        [TestCase(new int[] { }, new int[] { })]
+        [TestCase(new int[] { 1 }, new int[] { 1 })]
+        [TestCase(new int[] { 9, 8, 4, 1 }, new int[] { 9, 8, 4, 1 })]
+        [TestCase(new int[] { 1, 3, 3, 4 }, new int[] { 4, 3, 3, 1 })]
+        [TestCase(new int[] { 1, 7, 3, 4, 9 }, new int[] { 9, 7, 4, 3, 1 })]
+        public void SortDesc_WhenCalled_SortArrayDesc(int[] array, int[] expectedResult)
+        {
+            _insertionSort.SortDesc(array);
+
+            Assert.That(array, Is.EqualTo(expectedResult));
+        }
+    }
+}
---
+++ SortingAlgorithms/InsertionSort.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    public class InsertionSort
+    {
+        /// <summary>
+        /// Insertion Sort compares the current item with everything to the left of the array until it finds the right position to insert the current element.
+        /// In every comparison, if the element is bigger than current, it is copied to the right.
+        /// Time Complexity:
+        ///     [1] Best Case: Iteration O(n), Shift Item O(1) => O(n)
+        ///     [2] Worst Case: Iteration O(n), Shift Item O(n) => O of n square.
+        /// </summary>
+        /// <param name="array"></param>
+        public void SortAsc(int[] array)
+        {
+            if (array == null || array.Length == 0)
+                return;
+
+            for(int i = 1; i < array.Length; i++)
+            {
+                var current = array[i];
+                for(int j = i - 1; j >= 0; j--)
+                {
+                    if(current < array[j])
+                    {
+                        array[j + 1] = array[j];
+                        array[j] = current;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Mosh's solution uses a while loop instead of a for loop during comparison.
+        /// </summary>
+        /// <param name="array"></param>
+        public void SortAsc2(int[] array)
+        {
+            if (array == null || array.Length == 0)
+                return;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                var current = array[i];
+                var j = i - 1;
+                while(j >= 0 && array[j] > current)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j + 1] = current;
+            }
+        }
+
+        public void SortDesc(int[] array)
+        {
+            if (array == null || array.Length == 0)
+                return;
+
+            for(int i = 1; i < array.Length; i++)
+            {
+                var current = array[i];
+                var j = i - 1;
+                while (j >= 0 && array[j] < current)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j + 1] = current;
+            }
+        }
+    }
+}
