1b9a54c27151d6ab8d1346a68c6ecc7b80081403
Jack Hsu <jackhsu0225@gmail.com>
2020-03-08 20:55:47 -0400

    Added MergeSort methods.

---
+++ SortingAlgorithms.UnitTests/MergeSortTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+namespace SortingAlgorithms.UnitTests
+{
+    [TestFixture]
+    public class MergeSortTests
+    {
+        private MergeSort _mergeSort;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mergeSort = new MergeSort();
+
+        }
+
+        [Test]
+        [TestCase(null, null)]
+        [TestCase(new int[] { }, new int[] { })]
+        [TestCase(new int[] { 1 }, new int[] { 1 })]
+        [TestCase(new int[] { 9, 8, 4, 1 }, new int[] { 1, 4, 8, 9 })]
+        [TestCase(new int[] { 9, 4, 4, 1 }, new int[] { 1, 4, 4, 9 })]
+        [TestCase(new int[] { 1, 2, 3, 4 }, new int[] { 1, 2, 3, 4 })]
+        public void SortAsc_WhenCalled_SortArrayAsc(int[] array, int[] expectedResult)
+        {
+            _mergeSort.SortAsc(array);
+
+            Assert.That(array, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        [TestCase(null, null)]
+        [TestCase(new int[] { }, new int[] { })]
+        [TestCase(new int[] { 1 }, new int[] { 1 })]
+        [TestCase(new int[] { 9, 8, 4, 1 }, new int[] { 9, 8, 4, 1 })]
+        [TestCase(new int[] { 1, 3, 3, 4 }, new int[] { 4, 3, 3, 1 })]
+        [TestCase(new int[] { 1, 7, 3, 4, 9 }, new int[] { 9, 7, 4, 3, 1 })]
+        public void SortDesc_WhenCalled_SortArrayDesc(int[] array, int[] expectedResult)
+        {
+            _mergeSort.SortDesc(array);
+
+            Assert.That(array, Is.EqualTo(expectedResult));
+        }
+    }
+}
---
+++ SortingAlgorithms/MergeSort.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    public class MergeSort
+    {
+        /// <summary>
+        /// The real sorting actually happens at the merging stage.
+        /// Time Complexity: (Recursively divide the problem into half is O of logarithmic time)
+        ///     [1] Best Case: Dividing O of log n, Merging O of n => O of n log n.
+        ///     [2] Worst Case: Dividing O of log n, Merging O of n => O of n log n.
+        /// Space Complexity:
+        ///     O of n.
+        /// </summary>
+        /// <param name="array"></param>
+        public void SortAsc(int[] array)
+        {
+            //Base case
+            if (array == null || array.Length < 2)
+                return;
+
+            //Divide this array into half
+            var middle = array.Length / 2;
+
+            int[] left = new int[middle];
+            for(int i = 0; i < middle; i++)
+                left[i] = array[i];
+
+            int[] right = new int[array.Length - middle];
+            for(int i = middle; i < array.Length; i++)
+                right[i - middle] = array[i];
+
+            //Sort each half
+            SortAsc(left);
+            SortAsc(right);
+
+            //Merge the result
+            MergeAsc(left, right, array);
+        }
+
+        public void SortDesc(int[] array)
+        {
+            if (array == null || array.Length < 2)
+                return;
+
+            var middle = array.Length / 2;
+            var left = new int[middle];
+            for (int i = 0; i < middle; i++)
+                left[i] = array[i];
+
+            var right = new int[array.Length - middle];
+            for (int i = middle; i < array.Length; i++)
+                right[i - middle] = array[i];
+
+            SortDesc(left);
+            SortDesc(right);
+
+            MergeDesc(left, right, array);
+        }
+
+        private void MergeAsc(int[] left, int[] right, int[] result)
+        {
+            var i = 0;
+            var j = 0;
+            var k = 0;
+
+            while(i < left.Length && j < right.Length)
+            {
+                if (left[i] < right[j])
+                    result[k++] = left[i++];
+                else
+                    result[k++] = right[j++];
+            }
+
+            while(i < left.Length)
+                result[k++] = left[i++];
+
+            while (j < right.Length)
+                result[k++] = right[j++];
+        }
+
+        private void MergeDesc(int[] left, int[] right, int[] result)
+        {
+            int i = 0;
+            int j = 0;
+            int k = 0;
+
+            while (i < left.Length && j < right.Length)
+            {
+                if (left[i] > right[j])
+                    result[k++] = left[i++];
+                else
+                    result[k++] = right[j++];
+            }
+
+            while (i < left.Length)
+                result[k++] = left[i++];
+
+            while (j < right.Length)
+                result[k++] = right[j++];
+        }
+    }
+}
