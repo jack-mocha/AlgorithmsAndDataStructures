ff906ca867e5207d30b28559dd4c92fad51bfccd
jackhsu0225 <jackhsu0225@gmail.com>
2020-06-22 21:00:05 -0400

    Added more basic methods for BST and binary tree.

--- DataStructures/BinarySearchTree/Tree.cs
+++ DataStructures/BinarySearchTree/Tree.cs
@@ -65,10 +65,59 @@ namespace DataStructures
             }
         }
 
-        public bool Find(int value)
+        public void InsertRecursive(int value)
         {
+            InsertRecursive(root, null, value);
+        }
+
+        private void InsertRecursive(Node root, Node parent, int value)
+        {
+            if(root == null)
+            {
+                if (parent == null)
+                    this.root = new Node(value);
+                else if (value < parent.value)
+                    parent.leftChild = new Node(value);
+                else
+                    parent.rightChild = new Node(value);
+
+                return;
+            }
+
+            if (value < root.value)
+                InsertRecursive(root.leftChild, root, value);
+            else
+                InsertRecursive(root.rightChild, root, value);
+        }
+
+        public void Update()
+        {
+            Update(root, 0);
+        }
+        private void Update(Node root, int val)
+        {
+            if (root == null)
+                return;
+            else
+                root.value = val;
+
+            if (root.leftChild != null)
+            {
+                var newVal = 2 * val + 1;
+                Update(root.leftChild, newVal);
+            }
+            if(root.rightChild != null)
+            {
+                var newVal = 2 * val + 2;
+                Update(root.rightChild, newVal);
+            }
+        }
+
+        public bool FindIterative(int value)
+        {
+            //Iterative
             var current = root;
-            while(current != null)
+            while (current != null)
             {
                 if (value == current.value)
                     return true;
@@ -79,6 +128,23 @@ namespace DataStructures
             }
 
             return false;
+        }
+
+        //For BST 
+        public bool Find(int value)
+        {
+            return Find(root, value);
+        }
+
+        private bool Find(Node root, int target)
+        {
+            if (root == null)
+                return false;
+
+            if (target == root.value)
+                return true;
+
+            return Find(root.leftChild, target) || Find(root.rightChild, target);
         }
 
         public void TraversePreOrder()
@@ -101,6 +167,8 @@ namespace DataStructures
             TraverseInOrder();
         }
 
+        //This is in ascending order.
+        //If you want desc order, right, root, left
         private void TraverseInOrder(Node root)
         {
             if (root == null)
@@ -111,9 +179,10 @@ namespace DataStructures
             TraverseInOrder(root.rightChild);
         }
 
+        //This traverse from the leaf to root
         public void TraversePostOrder()
         {
-
+            TraversePostOrder(root);
         }
 
         private void TraversePostOrder(Node root)
@@ -126,6 +195,49 @@ namespace DataStructures
             Console.WriteLine(root.value);
         }
 
+        public int Depth(int target)
+        {
+            return Depth(root, target, 0);
+        }
+
+        //This works for Binary Tree and BST
+        private int Depth(Node root, int target, int depth)
+        {
+            if (root == null)
+                return -1;
+
+            if (target == root.value)
+                return depth;
+
+            return Math.Max(Depth(root.leftChild, target, depth + 1), Depth(root.rightChild, target, depth + 1));
+        }
+
+        //This only works for BST
+        public int BSTDepth(int target)
+        {
+            return BSTDepth(root, target);
+        }
+
+        private int BSTDepth(Node root, int target)
+        {
+            var depth = 0;
+            var current = root;
+            while(current != null)
+            {
+                if (target == current.value)
+                    return depth;
+
+                if (target < current.value)
+                    current = current.leftChild;
+                else
+                    current = current.rightChild;
+
+                depth++;
+            }
+
+            return -1; 
+        }
+
         //Height of a node is the longest distance between leaf and the node
         //Depth of a node is the distance between root and the node
         public int Height()
@@ -135,13 +247,220 @@ namespace DataStructures
 
         private int Height(Node root)
         {
-            if (root == null)
+            if (root == null) //When tree is empty.
                 return -1;
 
-            if (root.leftChild == null && root.rightChild == null)
+            if (IsLeaf(root))
                 return 0;
 
             return 1 + Math.Max(Height(root.leftChild), Height(root.rightChild));
+        }
+
+        private bool IsLeaf(Node node)
+        {
+            return node.leftChild == null && node.rightChild == null;
+        }
+
+        //This is post order traversal, because we are going from the leaf to the root.
+        //O(n)
+        //Find the min of the left sub tree and find the min of the right sub tree. Then compare them with the root.
+        private int Min(Node root)
+        {
+            if (root == null)
+                return Int32.MaxValue;
+
+            if (IsLeaf(root))
+                return root.value;
+
+            var left = Min(root.leftChild);
+            var right = Min(root.rightChild);
+
+            return Math.Min(Math.Min(left, right), root.value); //This is how to find min among 3 int
+        }
+
+        public int Min()
+        {
+            return Min(root);
+        }
+
+        public bool Equals(Tree other)
+        {
+            if (other == null)
+                return false;
+
+            return IsEqual(root, other.root);
+        }
+
+        //Pre-order Traversal
+        //Compare 2 nodes first and then compare the left and right subtrees.
+        private bool IsEqual(Node first, Node second)
+        {
+            if (first == null && second == null)
+                return true;
+            
+            if(first != null && second != null)
+                return first.value == second.value 
+                    && IsEqual(first.leftChild, second.leftChild) 
+                    && IsEqual(first.rightChild, second.rightChild);
+
+            return false;
+        }
+
+        public bool IsBST()
+        {
+            return IsBST(root, Int32.MinValue, Int32.MaxValue);
+        }
+
+        //Pre-Order and makes sure each node is within bound
+        private bool IsBST(Node root, int min, int max)
+        {
+            if (root == null)
+                return true;
+
+            if (root.value < min || root.value > max)
+                return false;
+
+            return IsBST(root.leftChild, min, root.value - 1) 
+                    && IsBST(root.rightChild, root.value + 1, max);
+        }
+
+        public List<int> GetNodesAtDistance(int distance)
+        {
+            var lst = new List<int>();
+            GetNodesAtDistance(root, distance, lst);
+
+            return lst;
+        }
+
+        //Pre-order and decrement the distance by 1
+        private void GetNodesAtDistance(Node root, int distance, List<int> lst)
+        {
+            if (root == null)
+                return;
+
+            if(distance == 0)
+            {
+                lst.Add(root.value);
+                return; //stop at the target
+            }
+
+            GetNodesAtDistance(root.leftChild, distance - 1, lst);
+            GetNodesAtDistance(root.rightChild, distance - 1, lst);
+        }
+
+        public void TraverseLevelOder()
+        {
+            for(var i = 0; i <= Height(); i++)
+            {
+                foreach(var l in GetNodesAtDistance(i))
+                    Console.WriteLine(l);
+            }
+        }
+
+        public int Size()
+        {
+            return Size(root);
+        }
+
+        //Size is the amount of nodes in the tree.
+        private int Size(Node root)
+        {
+            if (root == null)
+                return 0;
+
+            if (IsLeaf(root))
+                return 1;
+
+            return 1 + Size(root.leftChild) + Size(root.rightChild);
+
+        }
+
+        public int CountLeaves()
+        {
+            return CountLeaves(root);
+        }
+
+        private int CountLeaves(Node root)
+        {
+            if (root == null)
+                return 0;
+
+            if (IsLeaf(root))
+                return 1;
+
+            return CountLeaves(root.leftChild) + CountLeaves(root.rightChild);
+        }
+
+        public int Max()
+        {
+            if (root == null)
+                throw new InvalidOperationException();
+
+            return Max(root);
+        }
+
+        private int Max(Node root)
+        {
+            if (IsLeaf(root))
+                return root.value;
+
+            return Max(root.rightChild);
+        }
+
+        public bool Contains(int value)
+        {
+            return Contains(root, value);
+        }
+
+        private bool Contains(Node root, int value)
+        {
+            if (root == null)
+                return false;
+
+            if (root.value == value)
+                return true;
+
+            return Contains(root.leftChild, value) || Contains(root.rightChild, value);
+        }
+
+        public bool AreSibling(int first, int second)
+        {
+            return AreSibling(root, first, second);
+        }
+
+        private bool AreSibling(Node root, int first, int second)
+        {
+            if (root == null)
+                return false;
+
+            var areSibling = false;
+            if(root.leftChild != null && root.rightChild != null)
+            {
+                areSibling = (root.leftChild.value == first && root.rightChild.value == second)
+                        || (root.leftChild.value == second && root.rightChild.value == second);
+            }
+
+            return areSibling || AreSibling(root.leftChild, first, second) || AreSibling(root.rightChild, first, second);
+        }
+
+        //Greater Sum Tree
+        public void BstToGst()
+        {
+            BstToGst(root, 0);
+        }
+
+        private int BstToGst(Node root, int subTotal)
+        {
+            if (root == null)
+                return 0;
+
+            var sum = root.value + BstToGst(root.rightChild, subTotal);
+
+            root.value = sum + subTotal;
+
+            sum += BstToGst(root.leftChild, sum);
+
+            return sum;
         }
     }
 }
--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -7,17 +7,42 @@ namespace DataStructures
         public static void Main(string[] args)
         {
             var tree = new Tree();
-            tree.Insert(7);
-            tree.Insert(4);
-            tree.Insert(1);
-            tree.Insert(6);
-            tree.Insert(9);
-            tree.Insert(8);
-            tree.Insert(10);
-
+            var tree2 = new Tree();
+            int[] input = { 4, 1, 6, 0, 2, 5, 7, 3, 8 };
+            foreach(var i in input)
+            {
+                tree.InsertRecursive(i);
+                tree2.InsertRecursive(i);
+            }
+            var isEqual = tree.Equals(tree2);
+            //tree.Update();
+            var isFound = tree.Find(4);
+            isFound = tree.Find(1);
+            isFound = tree.Find(6);
+            isFound = tree.Find(0);
+            isFound = tree.Find(2);
+            isFound = tree.Find(5);
+            isFound = tree.Find(7);
+            isFound = tree.Find(3);
+            isFound = tree.Find(8);
+            isFound = tree.Find(10);
+            var min = tree.Min();
+            //tree.BstToGst();
+            //tree.Insert(7);
+            //tree.Insert(4);
+            //tree.Insert(1);
+            //tree.Insert(6);
+            //tree.Insert(9);
+            //tree.Insert(8);
+            //tree.Insert(10);
+            var depth = tree.Depth(99);
+            var bstDepth = tree.BSTDepth(2);
             var height = tree.Height();
-
-            
+            var k = tree.GetNodesAtDistance(2);
+            tree.TraverseLevelOder();
+            var size = tree.Size();
+            var leavesCount = tree.CountLeaves();
+            var max = tree.Max();
         }
     }
 }
